12f5653711952bd8b8f81964e5c1625971070158
Jordi Mas <jmas@src.gnome.org>
2008-03-01 19:03:22 +0000

    Win32 installer files
    svn path=/trunk/; revision=254

---
+++ platforms/win32/installer/gbrainy.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Running gbrainy
+set PATH=C:\mono\MonoX-1.2.1\bin;%PATH%
+set GTK_BASEPATH=C:\mono\MonoX-1.2.1
+set PKG_CONFIG_PATH=C:\mono\MonoX-1.2.1\lib\pkgconfig
+set GNOME_PATH=C:\mono\MonoX-1.2.1
+mono gbrainy.exe
