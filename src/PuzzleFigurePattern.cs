2d29af2e2d658071a9a5a2d8c6a9a020b9ff3e07
Jordi Mas <jmas@src.gnome.org>
2008-05-18 11:03:58 +0000

    Fixes
    svn path=/trunk/; revision=346

--- src/PuzzleFigurePattern.cs
+++ src/PuzzleFigurePattern.cs
@@ -38,7 +38,7 @@ public class PuzzleFigurePattern : Game
 	}
 
 	public override string Question {
-		get {return Catalog.GetString ("What figure should replace the question mark? Answer A, B or C");} 
+		get {return Catalog.GetString ("What figure should replace the question mark? Answer A, B or C.");} 
 	}
 
 	public override string Tip {
