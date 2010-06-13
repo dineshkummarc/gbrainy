1d280c8193e883ec14925a7475f32ed271d963c9
Jordi Mas <jmas@softcatala.org>
2010-06-14 00:46:20 +0200

    Support variables expression in games.xml

--- src/Core/Main/Xml/GameXmlFactory.cs
+++ src/Core/Main/Xml/GameXmlFactory.cs
@@ -20,8 +20,11 @@
 using System;
 using System.Xml;
 using System.IO;
+using System.Text;
 using System.Collections.Generic;
 using System.Globalization;
+using Mono.CSharp;
+using System.Text.RegularExpressions;
 
 using Mono.Unix;
 
@@ -31,6 +34,7 @@ namespace gbrainy.Core.Main
 	{
 		static List <GameXmlDefinition> games;
 		static bool read = false;
+		static bool? monofix_needed;
 
 		static GamesXmlFactory ()
 		{
@@ -69,10 +73,23 @@ namespace gbrainy.Core.Main
 				{
 					name = reader.Name.ToLower ();
 					switch (name) {
+					case "games":
+						break;
+					case "type":
+						break;
 					case "game":
 						if (reader.NodeType == XmlNodeType.Element) {
 							game = new GameXmlDefinition ();
 						} else if (reader.NodeType == XmlNodeType.EndElement) {
+
+							for (int i = 0; i < game.Variants.Count; i++)
+							{
+								game.Variants[i].Question = ReplaceVariables (game.Variants[i].Question);
+								game.Variants[i].Answer = ReplaceVariables (game.Variants[i].Answer);
+							}
+
+							game.Question = ReplaceVariables (game.Question);
+							game.Answer = ReplaceVariables (game.Answer);
 							games.Add (game);
 						}
 						break;
@@ -82,7 +99,7 @@ namespace gbrainy.Core.Main
 
 						game.Name = reader.ReadElementString ();
 						break;
-					case "_difficulty":
+					case "difficulty":
 						if (reader.NodeType != XmlNodeType.Element)
 							break;
 
@@ -189,7 +206,26 @@ namespace gbrainy.Core.Main
 							processing_variant = false;
 						}
 						break;
+					case "variables":
+					{
+						string eval;
+
+						// Using's for the variables section
+						eval = "using System;\n";
+						Mono.CSharp.Evaluator.Run (eval);
+
+						// Infrastructure for the user available
+						eval = "Random random = new Random ();\n";
+						Mono.CSharp.Evaluator.Run (eval);
+
+						eval = reader.ReadElementString ();
+						Mono.CSharp.Evaluator.Run (eval);
+						break;
+					}
 					default:
+						if (String.IsNullOrEmpty (name) == false)
+							Console.WriteLine ("GameXmlFactory. Unsupported tag: {0}", name);
+
 						break;
 					}
 				}
@@ -205,6 +241,112 @@ namespace gbrainy.Core.Main
 				read = true;
 				Console.WriteLine ("GameXmlFactory. Error loading: {0}", e.Message);
 			}
+		}
+
+		// Before Mono 2.6 (rev. 156533) there is no line separator between vars
+		static string FixGetVars (string str)
+		{
+			if (monofix_needed == null)
+			{
+				string eval, vars;
+
+				eval = "int a = 1; int b = 1;";
+				Evaluator.Run (eval);
+				vars = Evaluator.GetVars ();
+
+				monofix_needed = vars.IndexOf (System.Environment.NewLine) == -1;
+			}
+
+			if (monofix_needed == false)
+				return str;
+
+			// We just guarantee that int, doubles, and float are separated as modern Mono versions do
+			StringBuilder output = new StringBuilder ();
+			string [] keywords = new string [] {"int", "double", "float"};
+			int pos = 0, cur = 0;
+
+			while (pos != -1)
+			{
+				for (int i = 0; i < keywords.Length; i++)
+				{
+					pos = str.IndexOf (keywords [i], cur);
+					if (pos != -1)
+					{
+						output.Append (str.Substring (cur, pos - cur));
+						output.AppendLine ();
+						output.Append (str.Substring (pos, keywords[i].Length));
+						cur = pos + keywords[i].Length;
+						break;
+					}
+				}
+			}
+
+			output.Append (str.Substring (cur, str.Length - cur));
+			return output.ToString ();
+		}
+
+		static string GetVarValue (string vars, string _var)
+		{
+			const string exp = "([a-z0-9._%+-]+) ([a-z0-9._%+-]+) (=) ([0-9]+)";
+			Match match;
+			int idx, cur, newline_len;
+			string line;
+
+			Regex regex = new Regex (exp, RegexOptions.IgnoreCase);
+
+			newline_len = System.Environment.NewLine.Length;
+			cur = 0;
+
+			do
+			{
+				// Process a line
+				idx = vars.IndexOf (System.Environment.NewLine, cur);
+				if (idx == -1) idx = vars.Length;
+
+				line = vars.Substring (cur, idx - cur);
+				cur = idx + newline_len;
+				match = regex.Match (line);
+
+				//  "int num = 2";
+				//   group 1 -> int,  group 2 -> num,  group 3 -> =, group 4 -> 2
+				if (match.Groups.Count == 5)
+				{
+					if (match.Groups[2].Value == _var)
+						return match.Groups[4].Value;
+				}
+
+			} while (cur < vars.Length);
+
+			return string.Empty;
+		}
+
+		static string ReplaceVariables (string str)
+		{
+			const string exp = "\\[[a-z]+\\]+";
+			string eval, var, vars, var_value;
+			Regex regex;
+			Match match;
+
+			if (String.IsNullOrEmpty (str))
+				return str;
+
+			regex = new Regex (exp, RegexOptions.IgnoreCase);
+			match = regex.Match (str);
+
+			while (String.IsNullOrEmpty (match.Value) == false)
+			{
+				var = match.Value.Substring (1, match.Value.Length - 2);
+				vars = Evaluator.GetVars ();
+				vars = FixGetVars (vars);
+
+				var_value = GetVarValue (vars, var);
+
+				if (String.IsNullOrEmpty (var_value) == false)
+					str = str.Replace (match.Value, var_value);
+
+				match = match.NextMatch ();
+			}
+			return str;
 		}
 	}
 }
