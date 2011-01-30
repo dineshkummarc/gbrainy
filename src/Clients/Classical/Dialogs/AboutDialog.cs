7af39441738a110484c17cb00e73f904f8934131
Jordi Mas <jmas@softcatala.org>
2011-01-30 22:29:14 +0100

    Experimental web client reference

--- src/Clients/Classical/Dialogs/AboutDialog.cs
+++ src/Clients/Classical/Dialogs/AboutDialog.cs
@@ -76,8 +76,12 @@ namespace gbrainy.Clients.Classical.Dialogs
 			Copyright = Defines.COPYRIGHT;
 
 			Comments = Catalog.GetString ("A brain teaser game for fun and to keep your brain trained.");
+#if _ONLINE_
+			Comments += "\n";
+			Comments += String.Format (Catalog.GetString ("You can also play on-line at {0}"), Defines.WEB_SITE);
+#endif
 			Website = "http://live.gnome.org/gbrainy";
-			WebsiteLabel = Catalog.GetString ("gbrainy web site");
+			WebsiteLabel = Catalog.GetString ("gbrainy project web site");
 			TranslatorCredits = translators;
 			Artists = artists;
 			IconName = null;
